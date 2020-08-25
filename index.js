100251f68b63011c9422e35a7a1b508efc4aee79
Edward Stone <ed@autotelic.com>
2020-08-24 17:49:46 -0700

    add default params test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @flow
 
-const crypto = require('crypto');
+const crypto = require('crypto')
 
 type DecryptArgs = {
   ciphertext: string,
@@ -9,38 +9,38 @@ type DecryptArgs = {
 };
 
 const createEnvelopeEncrypter = keyService => {
-  const algorithm = 'aes256';
+  const algorithm = 'aes256'
 
-  const { getDataKey, decryptDataKey } = keyService;
+  const { getDataKey, decryptDataKey } = keyService
 
   const encrypt = async (plaintext: string) => {
-    const { encryptedKey, plaintextKey } = await getDataKey();
-    const salt = Buffer.from(crypto.randomBytes(8)).toString('hex');
-    const cipher = crypto.createCipheriv(algorithm, plaintextKey, salt);
+    const { encryptedKey, plaintextKey } = await getDataKey()
+    const salt = Buffer.from(crypto.randomBytes(8)).toString('hex')
+    const cipher = crypto.createCipheriv(algorithm, plaintextKey, salt)
     const ciphertext = [
       cipher.update(plaintext, 'utf8'),
-      cipher.final('hex'),
-    ].join('');
+      cipher.final('hex')
+    ].join('')
     return {
       ciphertext,
       key: encryptedKey,
-      salt,
-    };
-  };
+      salt
+    }
+  }
 
   const decrypt = async ({ ciphertext, key, salt }: DecryptArgs) => {
-    const dataKey = await decryptDataKey(key);
-    const decipher = crypto.createDecipheriv(algorithm, dataKey, salt);
+    const dataKey = await decryptDataKey(key)
+    const decipher = crypto.createDecipheriv(algorithm, dataKey, salt)
     return [
       decipher.update(ciphertext, 'hex', 'utf8'),
-      decipher.final('utf8'),
-    ].join('');
-  };
+      decipher.final('utf8')
+    ].join('')
+  }
 
   return {
     decrypt,
-    encrypt,
-  };
-};
+    encrypt
+  }
+}
 
-module.exports = createEnvelopeEncrypter;
+module.exports = createEnvelopeEncrypter
--- index.test.js
+++ index.test.js
@@ -1,31 +1,31 @@
-const t = require('tap');
-const crypto = require('crypto');
-const createEnvelopeEncrypter = require('.');
+const t = require('tap')
+const crypto = require('crypto')
+const createEnvelopeEncrypter = require('.')
 
 const plaintextKey = crypto
   .createHash('sha256')
   .update('password')
-  .digest();
+  .digest()
 
 const keyService = {
   getDataKey: () =>
     Promise.resolve({
       encryptedKey: 'foo',
-      plaintextKey,
+      plaintextKey
     }),
-  decryptDataKey: key => Promise.resolve(plaintextKey),
-};
+  decryptDataKey: key => Promise.resolve(plaintextKey)
+}
 
-const encrypter = createEnvelopeEncrypter(keyService);
+const encrypter = createEnvelopeEncrypter(keyService)
 
 t.test('envelope encrypter', async t => {
-  const result = await encrypter.encrypt('secret message');
-  const { key } = result;
-  t.equal(key, 'foo', 'encryptedKey is from keyService.getDataKey');
-  const secretMessage = await encrypter.decrypt(result);
+  const result = await encrypter.encrypt('secret message')
+  const { key } = result
+  t.equal(key, 'foo', 'encryptedKey is from keyService.getDataKey')
+  const secretMessage = await encrypter.decrypt(result)
   t.equal(
     secretMessage,
     'secret message',
-    'given ciphertext, key and salt decrypt returns plaintext message',
-  );
-});
+    'given ciphertext, key and salt decrypt returns plaintext message'
+  )
+})
