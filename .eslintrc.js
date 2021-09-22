2cfb181b51ae91fe5ebce31f6beb1e16fbb06691
Meghan Hein <meghan.hein@gmail.com>
2021-09-22 14:58:22 -0700

    chore: add publish and test github actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,18 @@ module.exports = {
   plugins: ['flowtype'],
   rules: {
     'flowtype/require-valid-file-annotation': [2, 'always'],
-    'import/prefer-default-export': 'off',
+    'import/prefer-default-export': 'off'
   },
   overrides: [
     {
       files: ['examples/**', '*.test.*', '**/test/**'],
       rules: {
         'flowtype/require-valid-file-annotation': 'off',
-        'flowtype/no-types-missing-file-annotation': 'off',
-      },
-    },
+        'flowtype/no-types-missing-file-annotation': 'off'
+      }
+    }
   ],
   env: {
-    node: true,
-  },
-};
+    node: true
+  }
+}
