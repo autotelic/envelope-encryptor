8c0c3bc86811cc07b0e1e70344e2f6baff41befb
Edward Stone <ed@autotelic.com>
2024-03-12 13:55:52 -0700

    fix key length in dummyKms

---
+++ examples/dummyKms/index.js
@@ -0,0 +1,14 @@
+import { createEnvelopeEncryptor, dummyKms } from '../../index.js'
+
+const kms = dummyKms()
+const { encrypt, decrypt } = createEnvelopeEncryptor(kms)
+
+const plaintext = 'Hello, world!'
+
+// encrypted is object containing ciphertext, key, and salt to store in db
+const encrypted = await encrypt(plaintext)
+
+// pass encrypted object to decrypt function to get plaintext
+const decrypted = await decrypt(encrypted)
+
+console.log(decrypted) // Hello, world!
