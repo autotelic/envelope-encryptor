386c4c7a3ce3d0c8a1bc6c7b2061a85fa6a4c33d
Edward Stone <ed@autotelic.com>
2024-03-12 14:00:58 -0700

    fix dummyKms example

--- examples/dummyKms/index.js
+++ examples/dummyKms/index.js
@@ -1,14 +1,16 @@
 import { createEnvelopeEncryptor, dummyKms } from '../../index.js'
 
-const kms = dummyKms()
-const { encrypt, decrypt } = createEnvelopeEncryptor(kms)
+;(async function () {
+  const kms = dummyKms()
+  const { encrypt, decrypt } = createEnvelopeEncryptor(kms)
 
-const plaintext = 'Hello, world!'
+  const plaintext = 'Hello, world!'
 
-// encrypted is object containing ciphertext, key, and salt to store in db
-const encrypted = await encrypt(plaintext)
+  // encrypted is object containing ciphertext, key, and salt to store in db
+  const encrypted = await encrypt(plaintext)
 
-// pass encrypted object to decrypt function to get plaintext
-const decrypted = await decrypt(encrypted)
+  // pass encrypted object to decrypt function to get plaintext
+  const decrypted = await decrypt(encrypted)
 
-console.log(decrypted) // Hello, world!
+  console.log(decrypted) // Hello, world!
+}())
