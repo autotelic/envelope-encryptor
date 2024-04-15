e4517fe09a02813792898e7e09da0a683ea78d5f
Holden Whitehead <holden@autotelic.com>
2024-04-15 13:04:57 -0700

    fix: handle Uint8Array from KMS CipherTextBlobs (#52)

--- index.d.ts
+++ index.d.ts
@@ -1,58 +1,47 @@
 declare module '@autotelic/envelope-encryptor' {
+  import { createCipheriv } from 'node:crypto'
+  import { KMS, KMSClientResolvedConfig } from '@aws-sdk/client-kms'
 
-  export interface AwsKmsConfig {
-    region?: string;
-    credentials?: {
-      accessKeyId?: string;
-      secretAccessKey?: string;
-    };
+  export type CipherKey = Parameters<typeof createCipheriv>[1]
+
+  export interface KeyService {
+    getDataKey: () => Promise<{
+      encryptedKey: string
+      plaintextKey: CipherKey
+    }>
+    decryptDataKey: (key: string) => Promise<CipherKey>
   }
 
-  export interface AwsKmsClient {
-    generateDataKey: (params: {
-      KeyId: string;
-      KeySpec: 'AES_256';
-    }) => { promise: () => Promise<{ CiphertextBlob: Buffer; Plaintext: Buffer }> };
+  export interface AwsKmsConfig extends KMSClientResolvedConfig {}
 
-    decrypt: (params: {
-      CiphertextBlob: Buffer;
-    }) => { promise: () => Promise<{ Plaintext: Buffer }> };
-  }
+  export type AwsKmsClient = Pick<KMS, 'generateDataKey' | 'decrypt'>
 
-  export interface AwsKmsService {
-    getDataKey: () => Promise<{ encryptedKey: string; plaintextKey: Buffer }>;
-    decryptDataKey: (key: string) => Promise<Buffer>;
-  }
+  export interface AwsKmsService extends KeyService {}
 
   export function awsKms(
     keyId: string,
     config?: AwsKmsConfig,
     Client?: new (config?: AwsKmsConfig) => AwsKmsClient
-  ): AwsKmsService;
-
-  export interface KeyService {
-    getDataKey: () => Promise<{ encryptedKey: string; plaintextKey: Buffer }>;
-    decryptDataKey: (key: string) => Promise<Buffer>;
-  }
+  ): AwsKmsService
 
   export interface EncryptorOptions {
-    encoding?: 'hex';
+    encoding?: 'hex'
   }
 
   export interface EncryptedData {
-    ciphertext: string;
-    key: Buffer;
-    salt: string;
+    ciphertext: string
+    key: string
+    salt: string
   }
 
   export interface EnvelopeEncryptor {
-    encrypt: (plaintext: string) => Promise<EncryptedData>;
-    decrypt: (data: EncryptedData) => Promise<string>;
+    encrypt: (plaintext: string) => Promise<EncryptedData>
+    decrypt: (data: EncryptedData) => Promise<string>
   }
 
   export function createEnvelopeEncryptor(
     keyService: KeyService,
     options?: EncryptorOptions
-  ): EnvelopeEncryptor;
+  ): EnvelopeEncryptor
 
 }
